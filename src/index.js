dabe40ef0d0de3236df29d50a768ad0a1ee69def
WalukaUI <cwaluka@yahoo.com>
2021-04-04 21:49:51 -0700

    Todolist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+ const dataget=document.getElementById('clickdata')
+ const list=[]
+
+  dataget.addEventListener('click',addingData)
+
+    function addingData(e){
+    e.preventDefault();
+    let uL=document.getElementById('tasks')
+    let newLi=document.createElement('li')
+    let newBtn=document.createElement('button')
+    
+    let all={content: document.getElementById('new-task-description').value}
+    list.push(all)
+  
+    for(let i = 0; i < list.length; i++){
+      uL.appendChild(newLi);
+      newLi.innerHTML=list[i].content;
+      document.getElementById('new-task-description').value="";
+    }
+
+    let Li=document.getElementsByTagName('li')
+    for(let i = 0; i < Li.length; i++){
+      newLi.appendChild(newBtn);
+      newBtn.innerHTML="Delete";
+      newBtn.onclick=deletes;
+      newBtn.className="close";
+      newBtn.id="newButton";
+    }
+  };
+
+function deletes(){
+
+  let close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+  deletes();
+
+
+
+})
+
