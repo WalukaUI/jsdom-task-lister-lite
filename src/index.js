87c3c7644c2e0ad21ac3b4fa7465417eebb54704
WalukaUI <cwaluka@yahoo.com>
2021-04-06 14:13:26 -0700

    beautyfy code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  const submitValue=document.getElementById('clickdata')
-  const pullValue=document.getElementById('new-task-description')
+  const submitValue = document.getElementById('clickdata')
+  const pullValue = document.getElementById('new-task-description')
 
-  const processingValue=submitValue.addEventListener('click',function(e){
+  submitValue.addEventListener('click', function (e) {
     e.preventDefault()
-    let values=[]
+    let values = []
     values.push(pullValue.value);
-    let uL=document.getElementById('tasks')
-    let lI=document.createElement('li')
-    let newBtn=document.createElement('button')
-    
-    for(let i=0;values.length>i;i++){
-    uL.appendChild(lI)
-    lI.innerHTML=values[i]
-    lI.appendChild(newBtn);
-    newBtn.innerHTML="Delete"
-    newBtn.className="close"
-    newBtn.onclick=deleteList;
-    pullValue.value=""
+    let uL = document.getElementById('tasks')
+    let lI = document.createElement('li')
+    let newBtn = document.createElement('button')
+
+    for (let i = 0; values.length > i; i++) {
+      uL.appendChild(lI)
+      lI.innerHTML = values[i]
+      lI.appendChild(newBtn);
+      newBtn.innerHTML = "Delete"
+      newBtn.className = "close"
+      newBtn.onclick = deleteList;
+      pullValue.value = ""
     }
   })
 
-  const deleteList= function(e){
+  const deleteList = function (e) {
     let close = document.getElementsByClassName("close")
-    for(let i=0;close.length>i;i++)
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
- 
+    for (let i = 0; close.length > i; i++)
+      close[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+
   }
 
 })
@@ -50,10 +50,10 @@ document.addEventListener("DOMContentLoaded", () => {
 //     let uL=document.getElementById('tasks')
 //     let newLi=document.createElement('li')
 //     let newBtn=document.createElement('button')
-    
+
 //     let all={content: document.getElementById('new-task-description').value}
 //     list.push(all)
-  
+
 //     for(let i = 0; i < list.length; i++){
 //       uL.appendChild(newLi);
 //       newLi.innerHTML=list[i].content;
